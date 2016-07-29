4d0f4b09065220faa611785022084aa34eebb6f8
lluisantoni <lluisantoni@gmail.com>
2016-07-29 12:27:33 -0500

    Adding last changes in code for all indices and changes in example files

--- FunctionsTest/SI_fo_example.m
+++ FunctionsTest/SI_fo_example.m
@@ -1,6 +1,6 @@
 clearvars
 
-abstol = 1e-3;
+abstol = 2*1e-2;
 reltol = 0e-2; % Pure absolute tolerance
 mmin = 10;
 mmax = 20; % I adjust that not to run out of memory. It can go up to 54. Type help cubSobol_SI_g for more information.
@@ -162,6 +162,54 @@ f = @(x) (1+1/d)^d*prod(x,2).^(1/d) +  pause_t(s);
 hyperbox = [zeros(1,d) ; ones(1,d)];
 
 R = [.1581948744, .1581948744, .1581948744, .1581948744, .1581948744, .1581948744; .1753745708, .1753745708, .1753745708, .1753745708, .1753745708, .1753745708];
+[q,app_int,out_param] = cubSobol_SI_fo_g(f,hyperbox,'abstol',abstol,'reltol',reltol,'mmin',mmin,'mmax',mmax,'fudge',@(m) fudge(m,d));
+SI = q;
+SI_n = out_param.n;
+SI_t = out_param.time;
+disp(SI)
+% disp(out_param.bound_err)
+disp(SI_n)
+% disp(SI_s)
+% disp(abs(R-SI) > max(abstol,R*reltol))
+% wronga = wronga + sum(sum(abs(R-SI) > max(abstol,R*reltol)));
+% disp(['Time:' num2str(SI_t)])
+% disp(['Wrong estimates with all:' num2str(wronga)]);
+
+% disp('Running Morokoff and Caflish example ...')
+% exitflag = [];
+% exitflagt = [];
+% for j = 1:d
+%     [q,app_int,out_param] = cubSobol_SI_g(f,hyperbox,j,'measure','uniform','abstol',abstol,'reltol',reltol,'mmin',mmin,'mmax',mmax,'fudge',fudge);
+%     exitflag = [exitflag out_param.exitflag];
+%     SI(1,j) = q;
+%     SI_n(1,j) = out_param.n;
+%     SI_t(1,j) = out_param.time;
+%     SI_s(1,j) = out_param.small;
+%     [q,app_int,out_param] = cubSobol_SI_g(f,hyperbox,-j,'measure','uniform','abstol',abstol,'reltol',reltol,'mmin',mmin,'mmax',mmax,'fudge',fudge);
+%     exitflagt = [exitflagt out_param.exitflag];
+%     SI(2,j) = q;
+%     SI_n(2,j) = out_param.n;
+%     SI_t(2,j) = out_param.time;
+%     SI_s(2,j) = out_param.small;
+% end
+% disp(SI)
+% disp(SI_n)
+% disp(SI_s)
+% disp(abs(R-SI) > abstol)
+% wrong = wrong + sum(sum(abs(R-SI) > abstol));
+% if any(sum(exitflag) > 0) || any(sum(exitflagt) > 0)
+%     warning('Results cannot be guaranteed due to reaching maximum budget or failing necessary conditions.')
+% end
+% disp(['Time:' num2str(sum(sum(SI_t)))])
+% disp(['Wrong estimates one by one:' num2str(wrong)]);
+
+%% Example Laurent
+disp('Running Laurents example ...')
+d = 6;
+f = @(x) (x(:,3).^3).*exp(x(:,4)).*(x(:,5).^2).*(x(:,2).^3.*x(:,3)+sin(x(:,4)+1).*x(:,6))./(1+x(:,1)).^(1/2);
+hyperbox = [zeros(1,d) ; ones(1,d)];
+
+R = [0.1949202354e-2, 0.2167739767e-1, .2765283730, 0.1658947475e-1, .1561007328, 0.3242382766e-1 0.118205783e-1, .1184436834, .7035364275, 0.934641572e-1, .5311670735, .1464118928];
 [q,app_int,out_param] = cubSobol_SI_fo_g(f,hyperbox,'abstol',abstol,'reltol',reltol,'mmin',mmin,'mmax',mmax,'fudge',@(m) fudge(m,d));
 SI = q;
 SI_n = out_param.n;
